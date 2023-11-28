c335c5c2d448a157d753278f86699ee1b2ef7848
Emily Barkley <149705329+daaap@users.noreply.github.com>
2023-11-28 16:10:59 -0700

    Update SignInHandler.java
    Made it so that it can function with the two factor authentication program.

--- EffortLoggerPrototypeALMOST/src/application/SignInHandler.java
+++ EffortLoggerPrototypeALMOST/src/application/SignInHandler.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,9 +16,12 @@ import javafx.stage.Stage;
 
 public class SignInHandler extends sceneController {
 	
+	// Default Constructor
 	public SignInHandler() {
-	
+		
 	}
+	
+	int authCode = 0;
 	
 	// Private variables for the varying buttons, labels, and fields
 	@FXML
@@ -36,8 +40,9 @@ public class SignInHandler extends sceneController {
 		checkCredentials(event);
 	}
 	
+	// Method that checks the credentials entered by the user in the login screen
 	private void checkCredentials(ActionEvent event) throws IOException { // A method that will see if the credentials are valid
-		//Main m = new Main();
+		Main m = new Main();
 
 		String[][] credentials = { // A variation of possible username and password combinations that would be accepted in the login screen
 				{"bongo", "orange1o!"}, 
@@ -45,34 +50,32 @@ public class SignInHandler extends sceneController {
 				{"sugarpie", "87*65"},
 				{"a", "a"}
 		};
-		//would instead call UserAccounts for the list of users. 
 		
 		boolean validCredentials = false; // A boolean variable set to false
 		
-		//System.out.println("made it here");
-		
+		// Checks to see if the entered credentials are valid
 		for(String [] credential : credentials) { // Goes through the credentials string array and checks if the login information matches
 			if(username.getText().toString().equals(credential[0]) && password.getText().toString().equals(credential[1])) {
-				//DP want to set active
-				
-				
 				validCredentials = true; // If it does match, set validCredentials to true and break out of the if statement
 				break;
 			}
 		}
+		
+		// If they are valid, load the next scene
 		if(validCredentials) { // Checks the boolean validCredentials
 			//wrongSignIn.setText("Welcome to Effortlogger."); // If it is true, it will welcome the user into the Effortlogger application
 			//m.changeStage("EffortConsole.fxml"); // Takes them to the second scene (Effortlogger)
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("EffortConsole.fxml"));
 			root = loader.load();
+	
+			//effortConsoleController controller = loader.getController();
+			//controller.setUsername(username.getText());
+						
+			// Create the authentication code for the user
+			authCode = (int)(Math.random() * 90000) + 10000;
+			System.out.println("Your 5 digit code is: " + authCode);
 			
-			effortConsoleController controller = loader.getController();
-			controller.setUsername(username.getText());
-			
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
+			switchToTwoFactorAuthen(event, authCode); // Switch scenes
 		}
 		else if(username.getText().isEmpty() && password.getText().isEmpty()) { // If the user didn't input anything, then ask them to enter their information
 			wrongSignIn.setText("Please enter your information.");
@@ -82,8 +85,3 @@ public class SignInHandler extends sceneController {
 		}
 	}
 }
-
-// Some things that can be added
-// A limited number of password tries
-// A more extensive database
-// A "forgot password" section where a verified user can reset their password securely
